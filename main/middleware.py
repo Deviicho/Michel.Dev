564705c25fd0e7bec59b7460623c0b3ca043e4fc
Deviicho <devviicho@gmail.com>
2025-06-16 18:30:34 +0300

    added an insta visitor detector

--- main/middleware.py
+++ main/middleware.py
@@ -9,10 +9,17 @@ class DiscordNotificationMiddleware:
         response = self.get_response(request)
         if request.path == '/':  # Notify only on homepage visits
             self.send_discord_notification(request)
+        if 'fbclid' in request.GET:
+            self.send_discord_notification2(request)
         return response
 
     def send_discord_notification(self, request):
         webhook_url = config('WEBHOOK_URL')
         ip = request.META.get('REMOTE_ADDR', 'unknown IP')
         message = f"@everyone New visit on michel.dev from {ip}"
+        requests.post(webhook_url, json={"content": message})
+        
+    def send_discord_notification2(self):
+        webhook_url = config('WEBHOOK_URL')
+        message = f"@everyone New visit on michel.dev from instagram !"
         requests.post(webhook_url, json={"content": message})
